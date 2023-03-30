b2e8e9f474e3a11d0004ddaaad0aff3d61396ec2
cecilruhe <129363654+cecilruhe@users.noreply.github.com>
2023-03-30 12:25:08 +0200

    Change2

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <h1> Welkom op de beste webshop</h1>
 
         <p> Vul hieronder jouw naam en e-mailadres in</p>
-
-<form action="welcome_get.php" method="get">
-Name: <input type="text" name="name"><br><br>
-E-mail: <input type="text" name="email"><br><br>
+        
+        <form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
