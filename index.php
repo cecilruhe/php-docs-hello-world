fe18b7361d31fd43b4a228c230b45f4a33987c9d
cecilruhe <129363654+cecilruhe@users.noreply.github.com>
2023-03-30 13:56:54 +0200

    ijklj

--- index.php
+++ index.php
@@ -4,10 +4,17 @@
     <h1> Welkom op de beste webshop</h1>
 
         <p> Vul hieronder jouw naam en e-mailadres in</p>
+
         
-        <form action="welcome.php" method="post">
+<form action="welcome.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+
 <input type="submit">
 </form>
 
