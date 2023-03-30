abf719ddafc93cccad5c4b52484d857c9ab90776
cecilruhe <129363654+cecilruhe@users.noreply.github.com>
2023-03-30 14:39:32 +0200

    sdds

--- welcome.php
+++ welcome.php
@@ -2,8 +2,8 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-Your gender is: <?php echo $_POST["gender"]; ?>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your gender is: <?php echo $_POST["gender"]; ?><br>
 You commented: <?php echo $_POST["comment"]; ?>
 
 </body>
